fe962bab9357fe9673b789db09cd61635ad04174
Meg McGregor <megmcgregor1@gmail.com>
2021-05-25 12:38:02 -0700

    phase2

--- Program.cs
+++ Program.cs
@@ -6,6 +6,22 @@ void Main()
 {
     Console.WriteLine("Guess a number!");
     string UserAnswer = Console.ReadLine();
-    Console.WriteLine($"{UserAnswer} is your answer!");
+    // Console.WriteLine($"{UserAnswer} is your answer!");
+
+    string SecretNumber = "42";
+
+    void GuessSecretNumber()
+    {
+        if (SecretNumber == UserAnswer)
+        {
+            Console.WriteLine("Bingo!");
+        }
+        else
+        {
+            Console.WriteLine("You should try again :(!");
+        }
+    }
+
+    GuessSecretNumber();
 }
 
